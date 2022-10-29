b1a1ee9ddd767d2a548ab95eb205b2a844bb2d12
Samuel Ibanga <ibangajnr27@gmail.com>
2022-10-29 08:16:04 +0100

    Updated urls.py

--- api_hosting/api/urls.py
+++ api_hosting/api/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path("data/", DataAPIView.as_view()),
+    path("data", DataAPIView.as_view()),
     path("data/<int:pk>/", GetDataAPIView.as_view())
 ]
